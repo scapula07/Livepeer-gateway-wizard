b7942328ab2b0914c43e34f69e497ed1554aae31
scapula07 <bartholomewonogwu@yahoo.com>
2024-09-05 01:42:53 +0100

    favicon

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
-
+import { IoBalloon } from "react-icons/io5";
 
 export default function Header() {
   return (
     <div className='w-full fixed px-10 py-4 font-mono italic z-20 bg-white border-b'>
          <div className='flex items-center w-full'>
              <div className='w-1/2'>
-                 <h5 className='font-semibold text-lg'>GATEWAY WIZARD</h5>
+                 
+                 <h5 className='font-semibold text-lg flex items-center'>
+                  <IoBalloon 
+                   className='text-green-500'
+                  />
+                  <span> GATEWAY WIZARD</span>
+                 
+                </h5>
 
              </div>
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta charSet='UTF-8' />
+        <meta name='keywords' content='GWID' />
+        <meta name='description' content={`GWID`} />
+        <link rel='icon' href='/balloon.png' />    
+      </Head>
       <body>
         <Main />
         <NextScript />
