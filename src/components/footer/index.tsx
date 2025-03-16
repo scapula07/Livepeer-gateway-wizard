1b929dae42e6b4a353046e54d2d6086f36ce3940
dnor-dev <anorue4rss@hotmail.com>
2025-03-16 19:46:25 +0100

    chore: refactor landing page header and history page components

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,75 +1,61 @@
-import React from 'react'
-import { FaGithub,FaLinkedin } from "react-icons/fa";
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
-export default function Footer() {  
+export default function Footer() {
   return (
-    <div className='w-full flex flex-col items-center pt-5 pb-4 px-10 bg-[#344E41] text-white'>
-        <div className='w-full justify-between flex py-7'>
-            {[
-               
-                {
-                    label:"Quick Links",
-                    links:[
-                        {
-                            text:"About GWID"
-                        },
-                        {
-                            text:"About Livepeer.org"
-                        },
-                        {
-                            text:"Blog"
-                        },
-                        {
-                            text:"Livepeer Discord"
-                        }  
-                    ]
-                },
-                {
-                    label:"Contact",
-                    links:[
-                        {
-                            text:"hello@gwid.io"
-                        },
-                      
-                    ]     
-                }
-
-             ]?.map((item)=>{
-                return(
-                    <div className='flex flex-col space-y-10'>
-                        <h5 className='font-semibold'>{item?.label}</h5>
-                         <div className='flex flex-col'>
-                            {item?.links?.map((link)=>{
-                                return(
-                                    <h5 className='text-white text-sm'>{link?.text}</h5>
-                                )
-                            })
-                            }
-                        </div>
-
-                    </div>
-                )
-            })
-
-            }
-
-        </div>
-
-        <div className='w-full flex border-t justify-between py-6'>
-            <div></div>
-            <div className='flex items-center space-x-3'>
-                <FaGithub
-                 className='text-2xl'
-                 />
-                <FaLinkedin 
-                   className='text-2xl'
-                 />
-
+    <div className="w-full flex flex-col items-center pt-5 pb-4 px-10 bg-[#344E41] text-white">
+      <div className="w-full justify-between flex py-7">
+        {[
+          {
+            label: "Quick Links",
+            links: [
+              {
+                text: "About GWID",
+              },
+              {
+                text: "About Livepeer.org",
+              },
+              {
+                text: "Blog",
+              },
+              {
+                text: "Livepeer Discord",
+              },
+            ],
+          },
+          {
+            label: "Contact",
+            links: [
+              {
+                text: "hello@gwid.io",
+              },
+            ],
+          },
+        ]?.map((item, index) => {
+          return (
+            <div className="flex flex-col space-y-10" key={index}>
+              <h5 className="font-semibold">{item?.label}</h5>
+              <div className="flex flex-col">
+                {item?.links?.map((link, index) => {
+                  return (
+                    <h5 key={index} className="text-white text-sm">
+                      {link?.text}
+                    </h5>
+                  );
+                })}
+              </div>
             </div>
-            
+          );
+        })}
+      </div>
+
+      <div className="w-full flex border-t justify-between py-6">
+        <div></div>
+        <div className="flex items-center space-x-3">
+          <FaGithub className="text-2xl" />
+          <FaLinkedin className="text-2xl" />
         </div>
-
-
+      </div>
     </div>
-  )
+  );
 }
--- src/components/layout/panel.tsx
+++ src/components/layout/panel.tsx
@@ -1,159 +1,159 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import { AiOutlineGateway } from "react-icons/ai";
 import { CiSettings } from "react-icons/ci";
 import { TbWaveSawTool } from "react-icons/tb";
-import { MdHistory ,MdDashboardCustomize} from "react-icons/md";
-import { useRouter } from 'next/router';
-import Link from 'next/link';
+import { MdHistory, MdDashboardCustomize } from "react-icons/md";
+import { useRouter } from "next/router";
+import Link from "next/link";
 import { FaEthereum } from "react-icons/fa";
 import { FaDeleteLeft } from "react-icons/fa6";
-import {doc,onSnapshot} from "firebase/firestore"
-import { db } from '@/firebase/config';
-import {gatewayStore} from "../../recoil"
-import {useRecoilState} from 'recoil'
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "@/firebase/config";
+import { gatewayStore } from "../../recoil";
+import { useRecoilState } from "recoil";
 
-type GATEWAY={
-  id:string
-  creator:string,
-  createdAt:string,
-  title:string,
-  cover:string,
-  rpcUrl:string,
-  type:string,
-  status:string
-  ip:string,
-  dashboardUrl:string,
-  ethAddress:string
-}
+type GATEWAY = {
+  id: string;
+  creator: string;
+  createdAt: string;
+  title: string;
+  cover: string;
+  rpcUrl: string;
+  type: string;
+  status: string;
+  ip: string;
+  dashboardUrl: string;
+  ethAddress: string;
+};
 
 export default function Panel() {
-    const router = useRouter();
-    const pathname = router.pathname;
-    const { gateway_id } = router.query ;
-    const gatewayId=gateway_id as string
-    const [active,setActive]=useState(`/gateways/${gateway_id}/dashboard`)
-    const [globalGateway,setGlobalGateway]=useRecoilState(gatewayStore) 
-    const [gateway,setGateway]=useState<GATEWAY>()
-    useEffect(()=>{
-        
-      if(gatewayId?.length >0){
-          const unsub = onSnapshot(doc(db,"gateways",gatewayId), (doc) => { 
-            const date = new Date(doc?.data()?.createdAt?.seconds * 1000);
+  const router = useRouter();
+  const pathname = router.pathname;
+  const { gateway_id } = router.query;
+  const gatewayId = gateway_id as string;
+  const [active, setActive] = useState(`/gateways/${gateway_id}/dashboard`);
+  const [globalGateway, setGlobalGateway] = useRecoilState(gatewayStore);
+  const [gateway, setGateway] = useState<GATEWAY>();
+  useEffect(() => {
+    if (gatewayId?.length > 0) {
+      const unsub = onSnapshot(doc(db, "gateways", gatewayId), (doc) => {
+        const date = new Date(doc?.data()?.createdAt?.seconds * 1000);
 
-            // Format as a readable string
-            const formattedDate = date.toLocaleString();   
-              setGateway({
-                id:doc?.id,
-                creator:doc.data()?.creator,
-                createdAt:formattedDate,
-                title:doc?.data()?.title,
-                cover:doc?.data()?.cover,
-                rpcUrl:doc?.data()?.rpcUrl,
-                type:doc?.data()?.type,
-                status:doc?.data()?.status,
-                ip:doc?.data()?.ip,
-                dashboardUrl:doc?.data()?.dashboardUrl,
-                ethAddress:doc?.data()?.ethAddress
-              })
+        // Format as a readable string
+        const formattedDate = date.toLocaleString();
+        setGateway({
+          id: doc?.id,
+          creator: doc.data()?.creator,
+          createdAt: formattedDate,
+          title: doc?.data()?.title,
+          cover: doc?.data()?.cover,
+          rpcUrl: doc?.data()?.rpcUrl,
+          type: doc?.data()?.type,
+          status: doc?.data()?.status,
+          ip: doc?.data()?.ip,
+          dashboardUrl: doc?.data()?.dashboardUrl,
+          ethAddress: doc?.data()?.ethAddress,
+        });
 
-              setGlobalGateway({
-                id:doc?.id,
-                creator:doc.data()?.creator,
-                createdAt:formattedDate,
-                title:doc?.data()?.title,
-                cover:doc?.data()?.cover,
-                rpcUrl:doc?.data()?.rpcUrl,
-                type:doc?.data()?.type,
-                status:doc?.data()?.status,
-                ip:doc?.data()?.ip,
-                dashboardUrl:doc?.data()?.dashboardUrl,
-                ethAddress:doc?.data()?.ethAddress
-              })
-          });
-      }
-  },[gateway_id])
-  
+        setGlobalGateway({
+          id: doc?.id,
+          creator: doc.data()?.creator,
+          createdAt: formattedDate,
+          title: doc?.data()?.title,
+          cover: doc?.data()?.cover,
+          rpcUrl: doc?.data()?.rpcUrl,
+          type: doc?.data()?.type,
+          status: doc?.data()?.status,
+          ip: doc?.data()?.ip,
+          dashboardUrl: doc?.data()?.dashboardUrl,
+          ethAddress: doc?.data()?.ethAddress,
+        });
+      });
+    }
+  }, [gateway_id]);
+
   return (
-    <div className='w-full h-full flex flex-col font-mono bg-white'>
-          <div className='flex flex-col space-y-2 border-b py-2'>
-              {[  {
-                    icon:<AiOutlineGateway />,
-                    label:"Gateways",
-                    link:"/gateways"
-                  },
-                  {
-                    icon:< MdHistory />,
-                    label:"History",
-                    link:"/history"
-                  },
-                  {
-                    icon:< TbWaveSawTool/>,
-                    label:"Home",
-                    link:"/"
-                  },
-               
-                ].map((tab,i)=>{
-                return(
-                    <Link href={tab?.link}>
-                          <div 
-                            className={
-                              `  flex w-full items-center space-x-4 ${tab?.link===pathname?'bg-[#58815794] ':'bg-white'} 
+    <div className="w-full h-full flex flex-col font-mono bg-white">
+      <div className="flex flex-col space-y-2 border-b py-2">
+        {[
+          {
+            icon: <AiOutlineGateway />,
+            label: "Gateways",
+            link: "/gateways",
+          },
+          {
+            icon: <TbWaveSawTool />,
+            label: "Home",
+            link: "/",
+          },
+        ].map((tab, i) => {
+          return (
+            <Link href={tab?.link}>
+              <div
+                className={`  flex w-full items-center space-x-4 ${
+                  tab?.link === pathname ? "bg-[#58815794] " : "bg-white"
+                } 
                                  px-4 py-2 rounded-lg hover:bg-slate-100 hover:text-black
                               `}
-                            key={i} > 
-                              <h5 className='font-bold'>{tab?.icon}</h5> 
-                                <h5 className='font-bold text-[0.88rem] font-mono'>{tab?.label}</h5>
-                         </div>
-                    </Link>
-                     ) })
-                     }
-
+                key={i}
+              >
+                <h5 className="font-bold">{tab?.icon}</h5>
+                <h5 className="font-bold text-[0.88rem] font-mono">
+                  {tab?.label}
+                </h5>
               </div>
-              {gateway_id?.length !=undefined&&
-                  <div className='flex flex-col space-y-2 py-2'>
-                      <h5 className='text-lg font-semibold px-4'>{gateway?.title} Gateway</h5>
-                       {[  {
-                           icon:<MdDashboardCustomize />,
-                           label:"Dashboard",
-                           link:`/gateways/${gateway_id}/dashboard`
-                         },
-                         {
-                           icon:< FaEthereum/>,
-                           label:"Fund Gateway",
-                           link:`/gateways/${gateway_id}/fund`
-                         },
-                        //  {
-                        //    icon:< CiSettings/>,
-                        //    label:"Settings & Configuration",
-                        //    link:`/gateways/${gateway_id}/settings`
-                        //  },
-                         {
-                          icon:< FaDeleteLeft style={{color:"red"}}/>,
-                          label:"Terminate",
-                          link:`/gateways/${gateway_id}/terminate`
-                        },
-                      
-                       ].map((tab,i)=>{
-                       return(
-                           <Link href={tab?.link}>
-                                 <div 
-                                   className={
-                                     `  flex w-full items-center space-x-4 ${tab?.link===active?'bg-[#58815794] ':'bg-white'} 
+            </Link>
+          );
+        })}
+      </div>
+      {gateway_id?.length != undefined && (
+        <div className="flex flex-col space-y-2 py-2">
+          <h5 className="text-lg font-semibold px-4">
+            {gateway?.title} Gateway
+          </h5>
+          {[
+            {
+              icon: <MdDashboardCustomize />,
+              label: "Dashboard",
+              link: `/gateways/${gateway_id}/dashboard`,
+            },
+            {
+              icon: <FaEthereum />,
+              label: "Fund Gateway",
+              link: `/gateways/${gateway_id}/fund`,
+            },
+            //  {
+            //    icon:< CiSettings/>,
+            //    label:"Settings & Configuration",
+            //    link:`/gateways/${gateway_id}/settings`
+            //  },
+            {
+              icon: <FaDeleteLeft style={{ color: "red" }} />,
+              label: "Terminate",
+              link: `/gateways/${gateway_id}/terminate`,
+            },
+          ].map((tab, i) => {
+            return (
+              <Link href={tab?.link}>
+                <div
+                  className={`  flex w-full items-center space-x-4 ${
+                    tab?.link === active ? "bg-[#58815794] " : "bg-white"
+                  } 
                                         px-4 py-2 rounded-lg hover:bg-slate-100 hover:text-black
                                      `}
-                                   key={i} 
-                                   onClick={()=>setActive(tab?.link)}
-                                   > 
-                                     <h5 className='font-bold'>{tab?.icon}</h5> 
-                                       <h5 className='font-bold text-[0.88rem] font-mono'>{tab?.label}</h5>
-                                </div>
-                           </Link>
-                            ) })
-                            }
-       
+                  key={i}
+                  onClick={() => setActive(tab?.link)}
+                >
+                  <h5 className="font-bold">{tab?.icon}</h5>
+                  <h5 className="font-bold text-[0.88rem] font-mono">
+                    {tab?.label}
+                  </h5>
                 </div>
-             }
-       
+              </Link>
+            );
+          })}
+        </div>
+      )}
     </div>
-  )
+  );
 }
